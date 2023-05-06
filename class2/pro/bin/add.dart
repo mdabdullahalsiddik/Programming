e33fbfe0b3d88c246e9f439a013cb421477bd2c4
mdabdullahalsiddik <mabdullahalsiddik@gmail.com>
2023-05-06 15:58:18 -0700

    dart

---
+++ class2/pro/bin/add.dart
@@ -0,0 +1,15 @@
+/*
+2 ta int number value hisebe assign Kore seguli add,sub,mul,div(+,-,x,/)
+*/
+
+void main(List<String> args) {
+  int number1, number2, add, sub, mul;
+  number1 = 10;
+  number2 = 20;
+  add = number1 + number2;
+  sub = number1 - number2;
+  mul = number1 * number2;
+  double div = number1 / number2;
+
+  print("$add\n$sub\n$mul\n$div");
+}
---
+++ class2/pro/bin/pp.dart
@@ -0,0 +1,13 @@
+/*
+Tomar name, roll, registration er jnno variable create Kore print koro
+
+*/
+
+void main(List<String> args) {
+  var name, roll, registration;
+  name = "Md. Abdullah Al Siddik";
+  roll = 502057;
+  registration = 1000000001;
+
+  print("$name\n$roll\n$registration");
+}
